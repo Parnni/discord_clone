fe13377af063300f64f67f33863c423042164142
Parnni <prajapaticamps8976@gmail.com>
2023-03-23 12:10:55 +0530

    Add basic templates.

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("room/", views.room, name="room"),
+    path("room/<str:pk>/", views.room, name="room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+rooms = [{"id": 1, "name": "Hello"}, {"id": 2, "name": "Ola"}]
 
 
 def home(request):
-    return HttpResponse("This is Home!")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return HttpResponse("This is room")
+def room(request, pk):
+    context = {"room": list(filter(lambda room: room["id"] == int(pk), rooms))[0]}
+    return render(request, "base/room.html", context)

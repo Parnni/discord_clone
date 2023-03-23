85d4b93ce48b90d822ad080ade4eb0f091f343a7
Parnni <prajapaticamps8976@gmail.com>
2023-03-23 17:40:21 +0530

    Add view for room creation.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
+from base import forms
 from base.models import Room
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def home(request):
@@ -11,3 +12,18 @@ def home(request):
 def room(request, pk):
     context = {"room": Room.objects.get(id=pk)}
     return render(request, "base/room.html", context)
+
+
+def create_room(request):
+    form = forms.RoomForm()
+
+    # Verifying the data for POST.
+    if request.method == "POST":
+        form = forms.RoomForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/create_room.html", context)

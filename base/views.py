7d5ff0c267c2359e8734656850a879c7eaf081d4
Parnni <prajapaticamps8976@gmail.com>
2023-03-23 16:30:29 +0530

    Remove test data and use model object.

--- base/views.py
+++ base/views.py
@@ -1,13 +1,13 @@
+from base.models import Room
 from django.shortcuts import render
-
-rooms = [{"id": 1, "name": "Hello"}, {"id": 2, "name": "Ola"}]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    context = {"room": list(filter(lambda room: room["id"] == int(pk), rooms))[0]}
+    context = {"room": Room.objects.get(id=pk)}
     return render(request, "base/room.html", context)

e046e571cf32172b66ae1a140d92bfe91997e756
Parnni <prajapaticamps8976@gmail.com>
2023-03-23 13:04:34 +0530

    Create Message model. Create a Base Audit model for DRY.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseAudits(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Room(BaseAudits):
+    # host =
+    # topic =
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    # participants =
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+
+    def __str__(self):
+        # return f"Host: {self.host} - Topic: {self.topic} , Name: {self.name}"
+        return f"Name: {self.name}"
+
+
+class Message(BaseAudits):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
+    body = models.TextField()
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"

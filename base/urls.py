e083fe07f7850ed728b066fc598005aef18917bb
Parnni <prajapaticamps8976@gmail.com>
2023-03-23 17:39:43 +0530

    Add endpoint for room creation.

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("home/", views.home, name="home"),
+    path("room/create/", views.create_room, name="create-room"),
     path("room/<str:pk>/", views.room, name="room"),
 ]
